48a06ff61027ccbdb96347a1cfe1bdb8ad1b503d
Ramon Suarez Alvarez <rsalvarez@gmail.com>
2018-05-04 10:02:33 +0200

    Control de versión de aplicación desde el fichero de strings. La versión en el manifest y en el Acerca de dependen de strings.

--- app/src/main/java/com/superg280/dev/luanco/AboutActivity.java
+++ app/src/main/java/com/superg280/dev/luanco/AboutActivity.java
@@ -2,6 +2,7 @@ package com.superg280.dev.luanco;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView txVersion = findViewById( R.id.textView_version);
+
+        txVersion.setText(String.format("%s %s", getString(R.string.about_version), getString(R.string.luanco_version)));
     }
 }
