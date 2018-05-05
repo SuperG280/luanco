ac416ee9942b7c4cfb421e56b97f298793c175d5
Ramon Suarez Alvarez <rsalvarez@gmail.com>
2018-05-05 12:58:19 +0200

    Comento el código de la recepción de notificaciones porque no funciona muy bien. Aun así, si envío notificación desde firebase, llega igual con los textos de firebase. Lo dejo de momento pendiente, porque así como está no hace daño.

--- app/src/main/java/com/superg280/dev/luanco/LuancoFireBaseMessagingService.java
+++ app/src/main/java/com/superg280/dev/luanco/LuancoFireBaseMessagingService.java
@@ -1,11 +1,15 @@
 package com.superg280.dev.luanco;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -14,7 +18,7 @@ public class LuancoFireBaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage message) {
-        sendMyNotification(message.getNotification().getBody());
+        //sendMyNotification(message.getNotification().getBody());
     }
 
 
@@ -25,14 +29,44 @@ public class LuancoFireBaseMessagingService extends FirebaseMessagingService {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
-        Uri soundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_HIGH);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(android.R.drawable.ic_menu_info_details)
+                .setTicker("Hearty365")
+                //     .setPriority(Notification.PRIORITY_MAX)
+                .setContentTitle("Default notification")
+                .setContentText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+                .setContentInfo("Info")
+                .setContentIntent(pendingIntent);
+
+        notificationManager.notify(/*notification id*/1, notificationBuilder.build());
+        /*
+        Uri soundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Notification.Builder notificationBuilder = new Notification.Builder(this)
+                .setSmallIcon( android.R.drawable.ic_menu_info_details)
                 .setContentTitle("Luanco ingresos y gastos")
                 .setContentText(message)
                 .setAutoCancel(true)
                 .setSound(soundUri)
-                .setSmallIcon( R.drawable.ic_stat_location_city)
                 .setContentIntent(pendingIntent);
 
 
@@ -42,5 +76,7 @@ public class LuancoFireBaseMessagingService extends FirebaseMessagingService {
         try {
             notificationManager.notify(0, notificationBuilder.build());
         } catch ( Exception ex) {}
+
+        */
     }
 }
